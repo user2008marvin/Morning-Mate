a871a51b0745bb419d0450ec47f7116c3d2b6f04
user2008marvin <martinc@avaugcom.com>
2026-07-05 20:31:41 +0100

    Add persistent analytics tracking + admin dashboard, fix stripe.ts bug

--- morning-mate/client/src/App.tsx
+++ morning-mate/client/src/App.tsx
@@ -14,6 +14,7 @@ import Success from "./pages/Success";
 import Help from "./pages/Help";
 import ResetPassword from "./pages/ResetPassword";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Admin from "./pages/Admin";
 function Router() {
   return (
     <Switch>
@@ -26,6 +27,7 @@ function Router() {
       <Route path={"/privacy"}><Redirect to="/privacy-policy" /></Route>
       <Route path={"/privacy-policy"} component={PrivacyPolicy} />
       <Route path={"/reset-password"} component={ResetPassword} />
+      <Route path={"/admin"} component={Admin} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
     </Switch>
